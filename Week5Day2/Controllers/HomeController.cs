2c1e3a5bca4d45a4582b866bb6ae80c451b4af0e
Jim Weiss <jweiss@motor.com>
2016-07-12 13:54:20 -0400

    First Commit

--- Week5Day2/Controllers/HomeController.cs
+++ Week5Day2/Controllers/HomeController.cs
@@ -8,6 +8,8 @@ namespace Week5Day2.Controllers
 {
     public class HomeController : Controller
     {
+        MovieDBEntities db = new Week5Day2.MovieDBEntities();
+
         public ActionResult Index()
         {
             return View();
@@ -15,16 +17,26 @@ namespace Week5Day2.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Now doing inventory.";
 
-            return View();
+            Inventory inv = db.Inventories.Find(4);
+
+            return View(inv);
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+           
 
-            return View();
+            var rentals = from r in db.Rentals
+                          where r.CustomerID == 1 && r.DiskID == 2
+                          select r;
+
+            Rental rental = null;
+            if (rentals.Count() > 0)
+                rental = rentals.First();
+
+            return View(rental);
         }
     }
 }
